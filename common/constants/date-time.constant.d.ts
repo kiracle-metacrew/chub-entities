4c0b383d36eebbcefc006817cbc9adff8af9ad9f
kiracle-be <kiracle.le@metacrew.vn>
2024-06-14 11:07:27 +0700

    add folder common/constants

---
+++ common/constants/date-time.constant.d.ts
@@ -0,0 +1,5 @@
+export declare const FORMAT_DATE_TIME: {
+    DATE_TIME: string;
+    DATE_ONLY: string;
+    INCREMENT_DAY: string;
+};
---
+++ common/constants/date-time.constant.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FORMAT_DATE_TIME = void 0;
+exports.FORMAT_DATE_TIME = {
+    DATE_TIME: 'YYYY-MM-DD HH:mm:ss',
+    DATE_ONLY: 'YYYY-MM-DD',
+    INCREMENT_DAY: 'YYYYMMDD',
+};
+//# sourceMappingURL=date-time.constant.js.map
